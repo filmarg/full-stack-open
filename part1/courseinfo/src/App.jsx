fba024c78d222c8e8b499e2d45bad4e737ac68e8
Filip Margielewski <filip.ma@proton.me>
2024-04-19 17:46:19 +0200

    Add exercise 1.3
    To make it worse.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,32 +15,39 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part info={props.content[0]} />
-      <Part info={props.content[1]} />
-      <Part info={props.content[2]} />
+      <Part info={props.part1} />
+      <Part info={props.part2} />
+      <Part info={props.part3} />
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.content[0].exercises + props.content[1].exercises + props.content[2].exercises}</p>
+    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const content = [
-    { part: 'Fundamentals of React', exercises: 10 },
-    { part: 'Using props to pass data', exercises: 7 },
-    { part: 'State of a component', exercises: 14 }
-  ]
+  const part1 = {
+    part: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    part: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    part: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
