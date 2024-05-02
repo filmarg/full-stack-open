f062138c13bce8dbeb601f4d90c43d909433892d
Filip Margielewski <filip.ma@proton.me>
2024-05-02 17:20:38 +0200

    Add exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNoteSubmit = (e) => {
     e.preventDefault()
 
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber,
     }
     
     if (persons.some(person => JSON.stringify(person) === JSON.stringify(newPerson))) {
@@ -18,26 +23,25 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
   
   const handleNameChange = (e) => setNewName(e.target.value)
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
   
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleNoteSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name+person.number}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
