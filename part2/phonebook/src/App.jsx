383e9583abf57eeab500f5426917a4aaf6b8a412
Filip Margielewski <filip.ma@proton.me>
2024-05-02 18:17:19 +0200

    Add exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    },
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'},
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [query, setQuery] = useState('')
+  const personsShown = persons.filter(person =>
+    person.name.toLowerCase().includes(query.toLowerCase()))
 
   const handleNoteSubmit = (e) => {
     e.preventDefault()
@@ -26,13 +29,15 @@ const App = () => {
       setNewNumber('')
     }
   }
-  
   const handleNameChange = (e) => setNewName(e.target.value)
   const handleNumberChange = (e) => setNewNumber(e.target.value)
+  const handleQueryChange = (e) => setQuery(e.target.value)
   
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={query} onChange={handleQueryChange} /></div>
+      <h2>Add a new one</h2>
       <form onSubmit={handleNoteSubmit}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -40,7 +45,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsShown.map(person =>
           <li key={person.name+person.number}>{person.name} {person.number}</li>
         )}
       </ul>
