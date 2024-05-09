e46254c5cb881319b63c8f06655d9ead3ff0dd72
Filip Margielewski <filip.ma@proton.me>
2024-05-09 08:42:50 +0200

    Use object IDs
    The next exercise requires the use of IDs, I guess.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -15,16 +15,16 @@ const Person = ({ person }) => <li>{person.name} {person.number}</li>
 
 const Persons = ({ persons }) => (
   <ul>
-    {persons.map(person => <Person key={person.name+person.number} person={person} />)}
+    {persons.map(person => <Person key={person.id} person={person} />)}
   </ul>
 )
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'},
+    { name: 'Arto Hellas', number: '040-123456', id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3},
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4},
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -38,9 +38,10 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     }
     
-    if (persons.some(person => JSON.stringify(person) === JSON.stringify(newPerson))) {
+    if (persons.some(person => JSON.stringify(person) === JSON.stringify({...newPerson, id: person.id}))) {
       alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(newPerson))
