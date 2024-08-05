97872330dcd6bd96e1eda95b65507e51b1e71148
Filip Margielewski <filip.ma@proton.me>
2024-08-05 09:36:01 +0200

    Add more stuff from part 3
    Based on the folder structure of the next exercise, I guess I don't
    have to that strict and could've added everything from the backend
    from part 3.  I might delete some of it later.

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+// Connect to the DB
 
 const mongoUrl = process.env.MONGODB_URI
 console.log('Connecting to', mongoUrl)
@@ -13,6 +18,8 @@ mongoose.connect(mongoUrl)
     console.log('Error connecting to MongoDB:', err.message)
   })
 
+// Make a model
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -20,12 +27,26 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 })
 
+// Alter the model for it to suit our needs
+blogSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+    delete ret.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 const app = express()
 
+morgan.token('data', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+
 app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
+
+//========== API
 
 app.get('/api/blogs', (req, res) => {
   Blog
@@ -44,6 +65,35 @@ app.post('/api/blogs', (req, res) => {
       res.status(201).json(result)
     })
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+//========== Error handling
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err)
+
+  if (err.name === 'CastError') {
+    // Invalid object ID for Mongo
+    res.status(400).send({ error: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    // Schema constraint violation for Mongo
+    const message = Object.values(err.errors)
+      .reduce((acc, e) => acc.concat(e.message, ' | '), '')
+    res.status(400).send({ error: message })
+  } else {
+    // The default Express error handler
+    next(err)
+  }
+}
+
+app.use(errorHandler)
+
+//========== Run server
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
