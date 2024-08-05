776bdb030b1ead82647deaa98aa018d16a164b8b
Filip Margielewski <filip.ma@proton.me>
2024-08-05 12:35:21 +0200

    Ex. 4.2: add error handling to blogs.js

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,15 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
+blogsRouter.get('/', (req, res, next) => {
   Blog
     .find({})
     .then(blogs => {
       res.json(blogs)
     })
+    .catch(err => next(err))
 })
 
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', (req, res, next) => {
   const blog = new Blog(req.body)
 
   blog
@@ -17,6 +18,7 @@ blogsRouter.post('/', (req, res) => {
     .then(result => {
       res.status(201).json(result)
     })
+    .catch(err => next(err))
 })
 
 module.exports = blogsRouter
