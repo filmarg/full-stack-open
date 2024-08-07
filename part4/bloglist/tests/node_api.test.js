d122e2a778a2dda5d1e110544b77a442a17d6187
Filip Margielewski <filip.ma@proton.me>
2024-08-07 10:15:26 +0200

    Add exercise 4.8

---
+++ part4/bloglist/tests/node_api.test.js
@@ -0,0 +1,36 @@
+const { test, describe, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (const blog of helper.initBlogs) {
+    const blogDoc = new Blog(blog)
+    await blogDoc.save()
+  }
+})
+
+describe('blogs', () => {
+  test('are returned as JSON, in correct amount', async () => {
+    const res = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(
+      res.body.length,
+      helper.initBlogs.length
+    )
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
