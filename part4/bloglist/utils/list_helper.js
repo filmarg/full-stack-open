d60b0949699515a0190d41a77e6fb0dc30884bf3
Filip Margielewski <filip.ma@proton.me>
2024-08-11 16:52:52 +0200

    Refactor into 'maxKey'

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -34,17 +34,11 @@ const mostBlogs = (blogs) => {
   }
   
   const authors = _.countBy(blogs.map(b => b.author))
-  let max = 0, topAuthor = blogs[0]
-  for (a in authors) {
-    if (authors[a] > max) {
-      max = authors[a]
-      topAuthor = a
-    }
-  }
-  
+  const topAuthor = maxKey(authors)
+
   return {
     author: topAuthor,
-    blogs: max,
+    blogs: authors[topAuthor],
   }  
 }
 
@@ -56,15 +50,16 @@ const mostLikes = (blogs) => {
   const authors = {}
   blogs.forEach(b =>
     authors[b.author] = (authors[b.author] || 0) + b.likes)
-
-  const topAuthor = Object.keys(authors).reduce((a, b) =>
-    authors[a] > authors[b] ? a : b)
+  const topAuthor = maxKey(authors)
   
   return {
     author: topAuthor,
     likes: authors[topAuthor],
   }
 }
+
+const maxKey = (obj) =>
+      Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b)
 
 module.exports = {
   dummy,
