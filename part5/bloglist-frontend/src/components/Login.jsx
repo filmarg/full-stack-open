d99ff3a9d569959b6571c1a1bb3fb96bd625e9fa
Filip Margielewski <filip.ma@proton.me>
2024-08-13 10:41:16 +0200

    Hide password

--- part5/bloglist-frontend/src/components/Login.jsx
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -2,15 +2,19 @@ const Login = ({ onSubmit, username, password }) => (
   <form onSubmit={onSubmit}>
     <div>
       Username:
-      <input value={username.val}
-             type="text"
-             onChange={username.onChange} />
+      <input
+        type="text"
+        value={username.val}
+        onChange={username.onChange}
+      />
     </div>
     <div>
       Password:
-      <input value={password.val}
-             type="text"
-             onChange={password.onChange} />
+      <input
+        type="password"
+        value={password.val}
+        onChange={password.onChange}
+      />
     </div>
     <button type="submit">Log in</button>
   </form>
