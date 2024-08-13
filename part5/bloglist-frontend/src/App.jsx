35642e8147922e7817a0710ced1646183b54ae88
Filip Margielewski <filip.ma@proton.me>
2024-08-13 15:28:26 +0200

    Add exercise 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Notification from './components/Notification'
 import BlogForm from './components/BlogForm'
 import Blogs from './components/Blogs'
 import Login from './components/Login'
@@ -7,6 +8,7 @@ import loginService from './services/login'
 import blogService from './services/blogs'
 
 const App = () => {
+  const [info, setInfo] = useState({ message: null })
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -33,15 +35,23 @@ const App = () => {
   const handleLogin = async (e) => {
     e.preventDefault()
 
-    const user = await loginService.login({ username, password })
-    window.localStorage.setItem('loggedUser', JSON.stringify(user))
-    setUser(user)
-    setUsername('')
-    setPassword('')
-    blogService.setToken(user.token)
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+
+      setUsername('')
+      setPassword('')
+
+      displayNotification('confirmation', `${user.name} logged in`, 3000)
+    } catch (ex) {
+      displayNotification('error', `Failed: ${ex.response.data.error}`, 8000)
+    }
   }
   
   const handleLogout = () => {
+    // Delete the token (and more) from everywhere
     window.localStorage.removeItem('loggedUser')
     setUser(null)
     blogService.setToken(null)
@@ -52,20 +62,33 @@ const App = () => {
     
     const blog = { title, author, url }
     
-    const newBlog = await blogService.create(blog)
-    setBlogs(blogs.concat(newBlog))
-    setTitle('')
-    setAuthor('')
-    setUrl('')
+    try {
+      const newBlog = await blogService.create(blog)
+      setBlogs(blogs.concat(newBlog))
+
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+      displayNotification('confirmation', `Blog added: ${newBlog.title}`, 5000)
+    } catch (ex) {
+      displayNotification('error', `Failed: ${ex.response.data.error}`, 8000)
+    }
   }
   
   const handleChange = (setter) =>
         (e) => setter(e.target.value)
   
+  const displayNotification = (type, message, delay) => {
+    setInfo({ message, type })
+    setTimeout(() => setInfo({ message: null }), delay)
+  }
+  
   if (!user) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification info={info} />
         <Login
           onSubmit={handleLogin}
           username={{val: username, onChange: handleChange(setUsername)}}
@@ -78,6 +101,7 @@ const App = () => {
   return (
     <div>
       <h2>Blogs</h2>
+      <Notification info={info} />
       <Logout name={user.name} onClick={handleLogout} />
       <h3>Post a blog</h3>
       <BlogForm
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,24 @@
+const Notification = ({ info }) => {
+  if (!info.message){
+    return null
+  }
+  
+  const color = info.type === 'error' ? 'red' : 'green'
+  const style = {
+    color,
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+  
+  return (
+    <div style={style}>
+      {info.message}
+    </div>
+  )
+}
+
+export default Notification
