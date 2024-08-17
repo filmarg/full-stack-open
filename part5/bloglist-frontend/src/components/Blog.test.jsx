f52dacd2d513c29c05ea94ba284156a654b7dc21
Filip Margielewski <filip.ma@proton.me>
2024-08-17 19:04:40 +0200

    Add exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -19,9 +19,20 @@ describe('<Blog />', () => {
     username: 'jon_doe',
   }
   
+  const mockLikeHandler = vi.fn()
+  
+  beforeEach(() => {
+    mockLikeHandler.mockClear()
+    render(
+      <Blog
+        blog={blog}
+        user={appUser}
+        onLike={mockLikeHandler}
+      />
+    )
+  })
+  
   test('renders title and author only', () => {
-    render(<Blog blog={blog} user={appUser} />)
-
     screen.getByText('Teach Yourself Programming in Ten Years—Peter Norvig')
 
     const element = screen.queryByText('https://www.norvig.com/21-days.html')
@@ -29,13 +40,26 @@ describe('<Blog />', () => {
   })
 
   test('renders URL and likes after clicking "View" button', async () => {
-    render(<Blog blog={blog} user={appUser} />)
-    
     const user = userEvent.setup()
     const button = screen.getByText('View')
     await user.click(button)
 
     screen.getByText('URL: https://www.norvig.com/21-days.html')
     screen.getByText('Likes: 5')
+  })
+
+  test('calls `onLike` twice if like button clicked twice', async () => {
+    const user = userEvent.setup()
+
+    // Show details and the "like" button
+    const viewButton = screen.getByText('View')
+    await user.click(viewButton)
+    // Click the "like" button
+    const likeButton = screen.getByText('Like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    // expect(mockLikeHandler.mock.calls).toHaveLength(2)
+    expect(mockLikeHandler).toHaveBeenCalledTimes(2)
   })
 })
