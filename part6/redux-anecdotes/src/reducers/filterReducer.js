1fffb724af36ec46d094c36f0efd1ee976fe6a74
Filip Margielewski <filip.ma@proton.me>
2024-08-23 17:43:53 +0200

    Add exercise 6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,21 +1,14 @@
-const filterReducer = (state = '', action) => {
-  // console.log('state now: ', state)
-  switch (action.type) {
-  case 'SET_FILTER':
-    return action.payload
-  default:
-    return state
-  }
-}
+import { createSlice, current } from '@reduxjs/toolkit'
 
-const filterChange = (query) => {
-  return {
-    type: 'SET_FILTER',
-    payload: query,
-  }
-}
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
+      return action.payload
+    },
+  },
+})
 
-export {
-  filterChange,
-}
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
